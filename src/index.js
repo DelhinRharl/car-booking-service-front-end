46712d2a78a9742b7326e4cb29d77a35f186eae8
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-08 15:50:45 +0600

    Setup react-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 
@@ -9,7 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/some-path" element={<h1>Another page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
