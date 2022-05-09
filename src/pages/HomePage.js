885c74bca282e2174eef8e969125d3cd93a7ab24
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-09 22:35:47 +0600

    Add carousel component to the page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,17 @@
-const HomePage = () => <h1>This is a HomePage</h1>;
+import ResponsiveCarousel from '../components/Carousel';
+
+const HomePage = () => (
+  <section className="w-full md:w-4/5 md:py-20">
+    <div className="w-[98%] text-center mb-10">
+      <h1 className="font-bold text-2xl md:text-5xl md:mb-5">
+        FEATURED MODELS
+      </h1>
+      <p className="font-bold text-sm text-gray-500">
+        CHOOSE YOUR PREMIUM CAR.
+      </p>
+    </div>
+    <ResponsiveCarousel />
+  </section>
+);
 
 export default HomePage;
