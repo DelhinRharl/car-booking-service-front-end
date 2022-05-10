3ee5aa842f841d20fb2b83bb1c0f7608d5419d67
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-10 19:47:46 +0600

    Test the cars GET endpoint

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,29 @@
+import { useEffect } from 'react';
 import ResponsiveCarousel from '../components/Carousel';
 
-const HomePage = () => (
-  <section className="w-full md:w-4/5 md:py-20">
-    <div className="w-[98%] text-center mb-10">
-      <h1 className="font-bold text-2xl md:text-5xl md:mb-5">
-        FEATURED MODELS
-      </h1>
-      <p className="font-bold text-sm text-gray-500">
-        CHOOSE YOUR PREMIUM CAR.
-      </p>
-    </div>
-    <ResponsiveCarousel />
-  </section>
-);
+const HomePage = () => {
+  const fetchItems = async () => {
+    const res = await fetch('http://localhost:3000/api/v1/cars');
+    const data = await res.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <section className="w-full md:w-4/5 md:py-20">
+      <div className="w-[98%] text-center mb-10">
+        <h1 className="font-bold text-2xl md:text-5xl md:mb-5">
+          FEATURED MODELS
+        </h1>
+        <p className="font-bold text-sm text-gray-500">
+          CHOOSE YOUR PREMIUM CAR.
+        </p>
+      </div>
+      <ResponsiveCarousel />
+    </section>
+  );
+};
 
 export default HomePage;
