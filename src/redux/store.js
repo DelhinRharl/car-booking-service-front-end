7a6ae924b5d908eeb2f4b986b802d433d39a8df8
DelhinRharl <delhinrharl@gmail.com>
2022-05-11 16:01:33 +0100

    Add details reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice';
+import detailsReducer from './cars/detailsSlice';
 
 export default configureStore({
   reducer: {
     cars: carsReducer,
+    details: detailsReducer,
   },
 });
