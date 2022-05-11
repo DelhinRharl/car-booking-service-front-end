100db67396f876b9ffd23cb649119e4bacfc782f
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-11 21:46:33 +0600

    Add userReducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice';
+import userReducer from './users/userSlice';
 
 export default configureStore({
   reducer: {
     cars: carsReducer,
+    user: userReducer,
   },
 });
