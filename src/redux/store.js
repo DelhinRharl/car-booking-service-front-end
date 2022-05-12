f0e3a6a55ea6323cc810195033208a5cff7f3e2f
DelhinRharl <delhinrharl@gmail.com>
2022-05-12 13:39:21 +0100

    Remove console.log/delete details slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice';
-import detailsReducer from './cars/detailsSlice';
 
 export default configureStore({
   reducer: {
     cars: carsReducer,
-    details: detailsReducer,
   },
 });
