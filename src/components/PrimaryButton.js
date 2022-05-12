1269f304ddf69c037e4cb417130b97f5d6860d2a
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-12 14:13:22 +0600

    Modify the prop types for Primary button

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 
-const PrimaryButton = ({ children }) => (
+const PrimaryButton = ({ children, onClick }) => (
   <button
     type="button"
     className="uppercase font-bold bg-amber-500 hover:bg-amber-600 transition-colors py-3 px-6 md:py-5 md:px-12 rounded-xl md:text-2xl"
+    onClick={onClick}
   >
     {children}
   </button>
 );
 
-PrimaryButton.propTypes = { children: PropTypes.string };
-PrimaryButton.defaultProps = { children: '' };
+PrimaryButton.propTypes = {
+  children: PropTypes.string,
+  onClick: PropTypes.func,
+};
+PrimaryButton.defaultProps = { children: '', onClick: null };
 export default PrimaryButton;
