705ab7124126f84d213d7035238ca5f0b7f399e0
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-14 01:44:04 +0600

    Add all the form fields

--- src/pages/ManageCars.js
+++ src/pages/ManageCars.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useForm } from 'react-hook-form';
+import FormError from '../components/FormError';
 import PrimaryButton from '../components/PrimaryButton';
 
 const ManageCars = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -11,7 +16,7 @@ const ManageCars = () => {
 
   return (
     <section className="w-full md:w-4/5 pt-20 md:py-20 overflow-y-auto h-screen">
-      <h1 className="font-bold text-2xl md:text-5xl md:mb-5 text-center uppercase mb-10 md:mb-20">
+      <h1 className="font-bold text-2xl md:text-5xl text-center uppercase mb-10 md:mb-20">
         Manage cars
       </h1>
       <form
@@ -24,24 +29,42 @@ const ManageCars = () => {
           {...register('make', { required: true, maxLength: 50 })}
           placeholder="Car make"
         />
+        {errors.make && <FormError>Must fill out this field</FormError>}
         <input
           type="text"
           className="border-2 border-amber-500 rounded w-full p-1"
           placeholder="Car model"
           {...register('model', { required: true, maxLength: 50 })}
         />
+        {errors.model && <FormError>Must fill out this field</FormError>}
         <input
           type="number"
           className="border-2 border-amber-500 rounded w-full p-1"
           placeholder="Car year"
           {...register('year', { required: true, min: 1000, max: 3000 })}
         />
+        {errors.year && <FormError>{errors.year?.type}</FormError>}
+        <input
+          type="number"
+          className="border-2 border-amber-500 rounded w-full p-1"
+          placeholder="Car price"
+          {...register('price', { required: true, min: 1 })}
+        />
+        {errors.price && <FormError>{errors.price?.type}</FormError>}
+        <input
+          type="text"
+          className="border-2 border-amber-500 rounded w-full p-1"
+          placeholder="Car image link"
+          {...register('image', { required: true })}
+        />
+        {errors.image && <FormError>This field is required</FormError>}
         <textarea
           placeholder="Car description"
           className="border-2 border-amber-500 rounded w-full p-1"
           {...register('description', { required: true })}
           rows="8"
         />
+        {errors.description && <FormError>Must fill out this field</FormError>}
         <PrimaryButton btnType="submit">submit</PrimaryButton>
       </form>
     </section>
