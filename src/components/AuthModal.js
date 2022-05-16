5ef9d28662d336acb4f0b1c4abd500fd8d6082dc
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-05-16 20:43:20 +0200

    Show onFormSubmitMessage in case of errors

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -67,6 +67,18 @@ const AuthModal = ({ isLogin = true, closeModal }) => {
         {isLogin ? 'Login' : 'Sign up'}
       </h2>
 
+      {onFormSubmitMessage && (
+      <div className="bg-orange-100 border border-orange-400 text-orange-700 pl-4 pr-10 py-3 rounded fixed top-4 right-1/2 translate-x-1/2" role="alert">
+        <span className="block sm:inline">{onFormSubmitMessage}</span>
+        <span role="button" tabIndex={0} onClick={() => { setOnFormSubmitMessage(''); }} onKeyDown={() => { setOnFormSubmitMessage(''); }} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg className="fill-current h-6 w-6 text-orange-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </span>
+      </div>
+      )}
+
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col items-center md:items-start gap-4 md:gap-7"
